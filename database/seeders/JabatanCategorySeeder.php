080095be1a884730741fb025c2b70c6e3b6b5b23
Tri Agung J <tri.agung@varx.id>
2022-07-14 03:52:21 +0700

    test commit

---
+++ database/seeders/JabatanCategorySeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+// namespace Database\Seeders;
+
+// use App\Models\JabatanCategory;
+// use Illuminate\Database\Seeder;
+
+// class JabatanCategorySeeder extends Seeder
+// {
+//     /**
+//      * Run the database seeds.
+//      *
+//      * @return void
+//      */
+//     public function run()
+//     {
+//         JabatanCategory::create([
+//             'name' => 'Pengurus Inti'
+//         ]);
+
+//         JabatanCategory::create([
+//             'name' => 'Staff'
+//         ]);
+
+//         JabatanCategory::create([
+//             'name' => 'Anggota'
+//         ]);
+//     }
+// }
