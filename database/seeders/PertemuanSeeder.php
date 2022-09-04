fdc5b25423cbe60e81403652f0334ad8b59868c0
Tri Agung J <tri.agung@varx.id>
2022-09-04 22:15:26 +0700

    update seeder

--- database/seeders/PertemuanSeeder.php
+++ database/seeders/PertemuanSeeder.php
@@ -24,27 +24,27 @@ class PertemuanSeeder extends Seeder
             'name' => 'Sesi 1',
             'pertemuan_id' => $pertemuan1Uuid,
             'hari' => DayEnum::Senin,
-            'waktu' => '09:00 - 10.30'
+            'waktu' => '09:00 - 10:30'
         ]);
 
         Sesi::create([
             'name' => 'Sesi 2',
             'pertemuan_id' => $pertemuan1Uuid,
             'hari' => DayEnum::Senin,
-            'waktu' => '10:40 - 12.10'
+            'waktu' => '10:40 - 12:10'
         ]);
 
         Sesi::create([
             'name' => 'Sesi 3',
             'pertemuan_id' => $pertemuan1Uuid,
             'hari' => DayEnum::Senin,
-            'waktu' => '12:50 - 14.20'
+            'waktu' => '12:50 - 14:20'
         ]);
         Sesi::create([
             'name' => 'Sesi 4',
             'pertemuan_id' => $pertemuan1Uuid,
             'hari' => DayEnum::Senin,
-            'waktu' => '14:30 - 16.00'
+            'waktu' => '14:30 - 16:00'
         ]);
 
         // SELASA
@@ -52,27 +52,27 @@ class PertemuanSeeder extends Seeder
             'name' => 'Sesi 1',
             'pertemuan_id' => $pertemuan1Uuid,
             'hari' => DayEnum::Selasa,
-            'waktu' => '09:00 - 10.30'
+            'waktu' => '09:00 - 10:30'
         ]);
 
         Sesi::create([
             'name' => 'Sesi 2',
             'pertemuan_id' => $pertemuan1Uuid,
             'hari' => DayEnum::Selasa,
-            'waktu' => '10:40 - 12.10'
+            'waktu' => '10:40 - 12:10'
         ]);
 
         Sesi::create([
             'name' => 'Sesi 3',
             'pertemuan_id' => $pertemuan1Uuid,
             'hari' => DayEnum::Selasa,
-            'waktu' => '12:50 - 14.20'
+            'waktu' => '12:50 - 14:20'
         ]);
         Sesi::create([
             'name' => 'Sesi 4',
             'pertemuan_id' => $pertemuan1Uuid,
             'hari' => DayEnum::Selasa,
-            'waktu' => '14:30 - 16.00'
+            'waktu' => '14:30 - 16:00'
         ]);
 
         // RABU
@@ -80,13 +80,13 @@ class PertemuanSeeder extends Seeder
             'name' => 'Sesi 1',
             'pertemuan_id' => $pertemuan1Uuid,
             'hari' => DayEnum::Rabu,
-            'waktu' => '09:00 - 10.30'
+            'waktu' => '09:00 - 10:30'
         ]);
         Sesi::create([
             'name' => 'Sesi 2',
             'pertemuan_id' => $pertemuan1Uuid,
             'hari' => DayEnum::Rabu,
-            'waktu' => '10:40 - 12.10'
+            'waktu' => '10:40 - 12:10'
         ]);
 
 
@@ -99,13 +99,13 @@ class PertemuanSeeder extends Seeder
             'name' => 'Sesi 3',
             'pertemuan_id' => $pertemuan2Uuid,
             'hari' => DayEnum::Rabu,
-            'waktu' => '12:50 - 14.20'
+            'waktu' => '12:50 - 14:20'
         ]);
         Sesi::create([
             'name' => 'Sesi 4',
             'pertemuan_id' => $pertemuan2Uuid,
             'hari' => DayEnum::Rabu,
-            'waktu' => '14:30 - 16.00'
+            'waktu' => '14:30 - 16:00'
         ]);
 
         // KAMIS
@@ -113,27 +113,27 @@ class PertemuanSeeder extends Seeder
             'name' => 'Sesi 1',
             'pertemuan_id' => $pertemuan2Uuid,
             'hari' => DayEnum::Kamis,
-            'waktu' => '09:00 - 10.30'
+            'waktu' => '09:00 - 10:30'
         ]);
 
         Sesi::create([
             'name' => 'Sesi 2',
             'pertemuan_id' => $pertemuan2Uuid,
             'hari' => DayEnum::Kamis,
-            'waktu' => '10:40 - 12.10'
+            'waktu' => '10:40 - 12:10'
         ]);
 
         Sesi::create([
             'name' => 'Sesi 3',
             'pertemuan_id' => $pertemuan2Uuid,
             'hari' => DayEnum::Kamis,
-            'waktu' => '12:50 - 14.20'
+            'waktu' => '12:50 - 14:20'
         ]);
         Sesi::create([
             'name' => 'Sesi 4',
             'pertemuan_id' => $pertemuan2Uuid,
             'hari' => DayEnum::Kamis,
-            'waktu' => '14:30 - 16.00'
+            'waktu' => '14:30 - 16:00'
         ]);
 
         // JUMAT
@@ -141,27 +141,27 @@ class PertemuanSeeder extends Seeder
             'name' => 'Sesi 1',
             'pertemuan_id' => $pertemuan2Uuid,
             'hari' => DayEnum::Jumat,
-            'waktu' => '08:30 - 10.00'
+            'waktu' => '08:30 - 10:00'
         ]);
 
         Sesi::create([
             'name' => 'Sesi 2',
             'pertemuan_id' => $pertemuan2Uuid,
             'hari' => DayEnum::Jumat,
-            'waktu' => '10:10 - 11.40'
+            'waktu' => '10:10 - 11:40'
         ]);
 
         Sesi::create([
             'name' => 'Sesi 3',
             'pertemuan_id' => $pertemuan2Uuid,
             'hari' => DayEnum::Jumat,
-            'waktu' => '12:50 - 14.20'
+            'waktu' => '12:50 - 14:20'
         ]);
         Sesi::create([
             'name' => 'Sesi 4',
             'pertemuan_id' => $pertemuan2Uuid,
             'hari' => DayEnum::Jumat,
-            'waktu' => '14:30 - 16.00'
+            'waktu' => '14:30 - 16:00'
         ]);
     }
 }
