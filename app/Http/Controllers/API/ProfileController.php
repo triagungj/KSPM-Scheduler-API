a824a53008b895e3edc69be8cc9d2e2af01d0424
Tri Agung J <tri.agung@varx.id>
2022-11-08 00:04:36 +0700

    feat(profile): create profile update & fix some

--- app/Http/Controllers/API/ProfileController.php
+++ app/Http/Controllers/API/ProfileController.php
@@ -28,7 +28,7 @@ class ProfileController extends Controller
         } else {
             $data =
                 Partisipan::where('username', $user->username)->firstOrFail();
-            $jabatan = $data->jabatan;
+            $jabatan = $data->jabatans;
             $avatarUrl = $data->avatar_url != null
                 ? url('/image') . '/' . $data->avatar_url
                 : null;
@@ -39,7 +39,8 @@ class ProfileController extends Controller
                         'data' => [
                             'name' => $data->name,
                             'phone_number' => $data->phone_number,
-                            'jabatan' => $jabatan->name,
+                            'jabatan_id' => $data->jabatan_id,
+                            'jabatan' => $jabatan->name ?? null,
                             'member_id' => $data->member_id,
                             'avatar_url' => $avatarUrl,
                         ]
